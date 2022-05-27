9ada6f0f9022b4bd54b4017d5935e753462e2be1
DmitriyFirsov <78723252+DmitriyFirsov@users.noreply.github.com>
2022-05-27 21:52:28 +0500

    Update db/seeds.rb
    fix quotes
    
    Co-authored-by: Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 
 for user_n in 0..10
-  user = User.new(name: 'user_'+user_n.to_s, email: 'user_'+user_n.to_s+'@example.com')
+  user = User.new(name: "user_" + user_n.to_s, email: "user_" + user_n.to_s + "@example.com")
   for task_n in 0..10
     user.tasks.new(name: 'task name ' + task_n.to_s, description: 'some description ' + task_n.to_s )
   end
