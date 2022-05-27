2ef12c05f979a4466cd551d4d98cdf5ed623d44b
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-05-27 21:06:56 +0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+for user_n in 0..10
+  user = User.new(name: 'user_'+user_n.to_s, email: 'user_'+user_n.to_s+'@example.com')
+  for task_n in 0..10
+    user.tasks.new(name: 'task name ' + task_n.to_s, description: 'some description ' + task_n.to_s )
+  end
+  user.save
+end
