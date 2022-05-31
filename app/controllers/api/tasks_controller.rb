3d6ac1fd113429569fc3c0408fab75f334ab79f2
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-05-31 17:13:14 +0500

    fix

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -4,7 +4,7 @@ module Api
   # controller for tasks json api
   class TasksController < Api::ApplicationController
     def index
-      tasks = TaskService.new.task_list(params: params)
+      tasks = TaskService.new.task_list(params:)
       render json: tasks, include: :user
     end
   end
--- app/controllers/app/tasks_controller.rb
+++ app/controllers/app/tasks_controller.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
-
-module App
-  # controller for task rout
-  class TasksController < App::ApplicationController
-    def index
-      @tasks = TaskService.new.task_list(params: params)
-    end
+# controller for task rout
+class TasksController < ApplicationController
+  def index
+    @tasks = TaskService.new.task_list(params:)
   end
 end
