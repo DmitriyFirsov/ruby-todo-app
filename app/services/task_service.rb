3abd682b5ad3156253ea467f8f31ed0b28c97a56
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-07-11 19:03:54 +0500

    PaginationHelper

--- app/services/task_service.rb
+++ app/services/task_service.rb
@@ -1,21 +1,11 @@
 # frozen_string_literal: true
 
 class TaskService
+  include PaginationHelper
+
   def task_list(params)
     set_offset(Task, params)
       .eager_load(:user)
       .all
-  end
-
-  protected
-
-  def calc_offset(page, limit)
-    (page - 1) * limit
-  end
-
-  def set_offset(entity, params)
-    entity
-      .offset(calc_offset(params[:page], params[:limit]))
-      .limit(params[:limit])
   end
 end
