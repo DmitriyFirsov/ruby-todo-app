866999e0e8a9e2bd8deec0136be5fb67c5d76267
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-07-13 12:42:50 +0300

    migation

---
+++ db/migrate/20220713090056_authentication.rb
@@ -0,0 +1,11 @@
+class Authentication < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :password_hash, :string
+
+    execute <<-SQL
+         UPDATE users u set password_hash = '#{BCrypt::Password.create('123456')}'
+    SQL
+
+    change_column_null :users, :password_hash, false
+  end
+end
