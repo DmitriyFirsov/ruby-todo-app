bf019ea6a02ac68a39c346afee7416ff03864052
ghost <78723252+DmitriyFirsov@users.noreply.github.com>
2022-07-13 13:02:40 +0300

    password in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, :email, presence: true
+  include BCrypt
+
+  validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
 
   has_many :tasks
+
+  def password 
+    @password_hash
+  end
+
+  def password=(new_password) 
+    @password_hash = Password.create(new_password)
+  end
 end
